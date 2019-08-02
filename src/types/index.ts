4f1dcef572f13a51765a89b87a4ce8d751171506
lizz1 <lizz1@guahao.com>
2019-08-02 20:38:06 +0800

    feat: 格式化文件

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,19 @@
 export interface ILive {
-  province: string;
-  city: string;
-  adcode: string;
-  weather: string;
-  temperature: string;
-  winddirection: string;
-  windpower: string;
-  humidity: string;
-  reporttime: string;
+    province: string;
+    city: string;
+    adcode: string;
+    weather: string;
+    temperature: string;
+    winddirection: string;
+    windpower: string;
+    humidity: string;
+    reporttime: string;
 }
 
 export interface IWeatherResponse {
-  status: string;
-  count: string;
-  info: string;
-  infocode: string;
-  lives: ILive[];
+    status: string;
+    count: string;
+    info: string;
+    infocode: string;
+    lives: ILive[];
 }
