2821386032915c46bc20fff7c0ce4bcfa8b5fef7
lizz1 <lizz1@guahao.com>
2019-08-02 22:25:19 +0800

    feat: 分模块处理

--- src/factory.ts
+++ src/factory.ts
@@ -1,11 +1,11 @@
 import { IConfigData } from './types';
 
-const factory: Function = (configData: IConfigData) => (method: Function) => (city: string) => method({ ...configData, city })
+const factory: Function = (configData: IConfigData) => (method: Function) => (city: string) => method({ ...configData, city });
 
-const URL: string = 'http://restapi.amap.com/v3/weather/weatherInfo';
-const KEY: string = '5bf40bae03f0adf4f80b6c3b5dde89ad';
+const URL = 'http://restapi.amap.com/v3/weather/weatherInfo';
+const KEY = '5bf40bae03f0adf4f80b6c3b5dde89ad';
 
 export default factory({
-  url: URL,
-  key: KEY
+    url: URL,
+    key: KEY
 });
--- src/getWeather.ts
+++ src/getWeather.ts
@@ -7,30 +7,30 @@ import factory from './factory';
 const log = console.log;
 
 function getWeather(params: IParams) {
-  axios.get(`${params.url}?city=${encodeURI(params.city)}&key=${params.key}`).then((res: AxiosResponse<IWeatherResponse>) => {
+    axios.get(`${params.url}?city=${encodeURI(params.city)}&key=${params.key}`).then((res: AxiosResponse<IWeatherResponse>) => {
     // status 为 0 时表示查询失败
-    if (res.data.status === '0') {
-        log(colors.red('天气信息查询失败！'));
-        return;
-    }
+        if (res.data.status === '0') {
+            log(colors.red('天气信息查询失败！'));
+            return;
+        }
 
-    // count 为 0 表示没有查到天气信息
-    if (res.data.count === '0') {
-        log(colors.yellow('天气信息查询失败！'));
-        return;
-    }
+        // count 为 0 表示没有查到天气信息
+        if (res.data.count === '0') {
+            log(colors.yellow('天气信息查询失败！'));
+            return;
+        }
 
-    // 获取并输出第一条天气信息
-    const live: ILive = res.data.lives[0];
+        // 获取并输出第一条天气信息
+        const live: ILive = res.data.lives[0];
 
-    log('================天气预报 start====================');
-    log(colors.bold(`预报时间：`), colors.yellow(`${live.reporttime}`));
-    log(colors.bold(`预报地区：`), colors.white(`${live.province} ${live.city}`));
-    log(colors.bold(`预报详情：`), colors.green(`${live.weather} ${live.temperature} 度`));
-    log('================天气预报 end====================');
-  }).catch(() => {
-      log(colors.red('天气服务出现异常！'));
-  });
+        log('================天气预报 start====================');
+        log(colors.bold(`预报时间：`), colors.yellow(`${live.reporttime}`));
+        log(colors.bold(`预报地区：`), colors.white(`${live.province} ${live.city}`));
+        log(colors.bold(`预报详情：`), colors.green(`${live.weather} ${live.temperature} 度`));
+        log('================天气预报 end====================');
+    }).catch(() => {
+        log(colors.red('天气服务出现异常！'));
+    });
 }
 
-export default factory(getWeather)
+export default factory(getWeather);
